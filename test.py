43eb01fee03509684cb138a5eb2bb38f238d9482
Kushal N <kushalnjnv10@gmail.com>
2021-08-16 23:19:13 +0530

    added config file to gitignore

--- test.py
+++ test.py
@@ -1,25 +1,10 @@
 # This file is used for testing pieces of code 
 
 from iqoptionapi.stable_api import IQ_Option
+from configparser import ConfigParser
 import time
 
-print("imported all modules")
-trader = IQ_Option("kushalnjnv10@gmail.com", "Kushal#07")
+config = ConfigParser()
+config.read('config.ini')
 
-trader.connect()
-print("connected")
-ACTIVES="CHFJPY"
-
-def ChangeACTIVES():
-    openACTIVES = trader.get_all_profit()
-    open_list = {}
-    for openACTIVE in openACTIVES:
-        ACTIVE = (openACTIVES[openACTIVE])['turbo']
-        if not ACTIVE == {}:
-            open_list[openACTIVE] = ACTIVE
-
-    print(open_list)
-    max_key = max(open_list, key=open_list.get)
-    return(max_key)
-
-ChangeACTIVES()
+print(config['account']['email'], config['account']['password'])
