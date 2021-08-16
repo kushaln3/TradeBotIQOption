e1136c1644ba73f71a71c3bce32ef42954c20758
Kushal N <kushalnjnv10@gmail.com>
2021-08-16 15:59:04 +0530

    Basic Code with action switcher, increment, snd while loop

--- Main.py
+++ Main.py
@@ -1,2 +1,51 @@
-from iqoptionapi.stable_api import IQOptionAPI
+from iqoptionapi.stable_api import IQ_Option
+
+trader = IQ_Option("kushalnjnv10@gmail.com", "Kushal#07")
+
+trader.connect()
+
+
+def Result(id):
+    if trader.check_win_v3(id) >= 0:
+        return(True)
+    else:
+        return(False)
+
+
+
+def ChangedAction(ACTION):
+    if ACTION == "call":
+        return("put")
+    elif ACTION == "put":
+        return("call")
+
+
+
+
+while True:
+
+    Money=1
+
+    Increment = 2.0
+
+    ACTIVES="EURUSD"
+
+    ACTION="call"#or "put"
+
+    expirations_mode=1
+
+
+    while True:
+        check, id = trader.buy(Money, ACTIVES, ACTION, expirations_mode)
+
+        if check: print(f"{ACTION} order succcessful")
+        else: print(f"{ACTION} order failed")
+
+        result = Result(id)
+
+        if result:
+            break
+        else:
+            Money = round(Money*Increment)
+            ACTION = ChangedAction(ACTION)
 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Myprojects
-This the repository to store all my projects
+# IQ Option Binary Bot
+This Bot multiplies the amount to invest and places buy and sell orders, alternatively, highly risky, as it can double your money in a day or eat all your Money.
